bb1569152d055a35ed05d3c1728d41027785db80
kavisaru <64577758+kavisaru@users.noreply.github.com>
2020-10-18 06:28:20 +0000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,131 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is known as The Saint of Gutters?",
+    answer: "Mother Teresa",
+    options: [
+      "Baba Amte",
+      "Mother Teresa",
+      "Anna Hazare",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "For which of the following disciplines is Nobel Prize awarded?",
+    answer: "All of the above",
+    options: [
+      "Physics and Chemistry",
+      "Physiology or Medicine",
+      "Literature, Peace and Economics",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "FFC stands for?",
+    answer: "Film Finance Corporation",
+    options: [
+      "Foreign Finance Corporation",
+      "Film Finance Corporation",
+      "Federation of Football Council",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a member of the European Union?",
+    answer: "Norway",
+    options: [
+      "Greece",
+      "Finland",
+      "Norway",
+      "United Kingdom"
+    ]
+  },
+    {
+    numb: 5,
+    question: "DRDL stands for?",
+    answer: "Defence Research and Development Laboratary",
+    options: [
+      "Defence Research and Development Laboratary",
+      "Department of Research and Development Laboratory",
+      "Differential Research and Documentation Laboratary",
+      "None of the above"
+    ]
+  },
+   {
+
+    numb: 6,
+      question: "Which Indian state has its maximum area under the forest cover?",
+    answer: "Arunachal Pradesh",
+    options: [
+      "Maharashtra",
+      "Madhya Pradesh",
+      "Arunachal Pradesh",
+      "Kerala"
+    ]
+  },
+   {
+     numb: 7,
+      question: "The headquarters of Reserve Bank of India is…?",
+    answer: "Mumbai",
+    options: [
+      "Kolkata",
+      "New Delhi",
+      "Mumbai",
+      "Chennai"
+    ]
+  },
+ {
+
+    numb: 8,
+      question: "Eugenics is the study of?",
+    answer: "altering human beings by changing their genetic components",
+    options: [
+      "altering human beings by changing their genetic components",
+      "people of European origin",
+      "different races of mankind",
+      "genetic of plants"
+    ]
+  },
+ {
+
+    numb: 9,
+    question: "What causes filariasis?",
+    answer: "Mosquito",
+    options: [
+      "Bacteria",
+      "Mosquito",
+      "Protozoa",
+      "Virus"
+    ]
+  },
+ {
+
+    numb: 10,
+    question: "Mount Etna is a famous volcano which is located?",
+    answer: "Italy",
+    options: [
+      "Argentina",
+      "Italy",
+      "Mexico",
+      "Philipines"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
